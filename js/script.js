9beb4a2153a9b8d9fcfe3da586840afa9a0e4348
MassimilianoTusini <massimilianotusini110@gmail.com>
2025-09-08 16:34:53 +0200

    Manda i numeri casuali a schermo

--- js/script.js
+++ js/script.js
@@ -17,3 +17,41 @@
 // Individuate gli elementi di cui avete bisogno per realizzare il programma.,
 // Immaginate la logica come fosse uno snack: "Dati 2 array di numeri, indica quali e quanti numeri ci sono in comune tra i due array",
 
+
+// Dichiarazione degli input
+const myForm = document.getElementById ("answer-form");
+const mioUl = document.getElementById ("numbers-list");
+const countDown = document.getElementById("countdown")
+
+
+// Funzione per generare numeri casuali
+function casualNum(min, max){
+    return Math.floor(Math.random() * (max - min + 1)) + min; 
+}
+// Console log per vedere se la funzione è corretta
+console.log(casualNum(10, 50))
+
+// Funzione che genera un array di tot numeri in un range
+function arrayNum(minNum, maxNum, numElements){
+    // Variabile di array vuoto
+    let numeri = []
+    // Ciclo l'Array finchè è più corto di numElements (aggiungo elementi all'array finchè non ha raggiunto il numero di elementi che voglio mettere al suo interno)
+    while (numeri.length < numElements){
+        // Dichiaro una variabile che richiama la funzione usata per generare un numero casuale
+        const num = casualNum(minNum, maxNum);
+        // Se il numero NON è presente nel ciclo allora lo aggiungiamo all'array (verifica per evitare i doppi numeri)
+        if (!numeri.includes(num)){
+            numeri.push(num);
+        }
+    }
+    return numeri
+};
+// Dichiaro una costante con i valori da mandare a schermo tramite l'uso delle variabili (un numero da 1 a 50 ripetuto 5 volte)
+const numeriSchermo = arrayNum(1, 50, 5)
+// Mando a schermo i numeri generati casualmente dalla prima funzione e aggiunti nell'array dalla seconda funzione
+numeriSchermo.forEach(numero=> {
+    const mioLi = document.createElement("li");
+    mioLi.textContent = numero;
+    mioUl.appendChild(mioLi)
+})
+
